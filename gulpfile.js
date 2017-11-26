47252fce3f10a58d86f594206708712084da2d77
ogil0035 <32109851+ogil0035@users.noreply.github.com>
2017-11-26 13:39:25 -0500

    Sat Updates
    Updates for the day

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,50 @@
-// Declare global scope variables.
-const gulp = require('gulp')      // load the gulp library
-const sass = require('gulp-sass') // load the gulp-sass compiler library
-const sassPath = './scss/**/*.scss' // the folder(s) with our Sass source files
-const postcss = require('gulp-postcss') // load the postcss library
-const autoprefixer = require('autoprefixer') // load the autoprefixer plugin
-const cssnano = require('cssnano') // load the cssnano plugin
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var pkg = require('./package.json');
 
-// Define a new task called 'sass' that we can call to compile Sass to CSS
-gulp.task('sass', function () {
-  // Create a plugins variable with the names and configuration parameters
-  // of the PostCSS plugins that we want to use.
-  const plugins = [
-    autoprefixer({ browsers: ['last 2 version'] }),
-    cssnano()
-  ]
-  // Now lets tell gulp what to do ...
-  return gulp
-    .src(sassPath)            // where to find the Sass source files (use our variable)
-    .pipe(sass())             // forward those files to the compiler
-    .pipe(gulp.dest('./css')) // where to output the interim compiled css
-    .pipe(postcss(plugins))   // apply the PostCSS plugins
-    .pipe(gulp.dest('./css/min')) // where to output the final minimized CSS files
+// Copy vendor files from /node_modules into /vendor
+// NOTE: requires `npm install` before running!
+gulp.task('copy', function() {
+  gulp.src([
+      'node_modules/bootstrap/dist/**/*',
+      '!**/npm.js',
+      '!**/bootstrap-theme.*',
+      '!**/*.map'
+    ])
+    .pipe(gulp.dest('vendor/bootstrap'))
+
+  gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/jquery/dist/jquery.min.js'])
+    .pipe(gulp.dest('vendor/jquery'))
+
+  gulp.src(['node_modules/jquery.easing/*.js'])
+    .pipe(gulp.dest('vendor/jquery-easing'))
+
+  gulp.src([
+      'node_modules/font-awesome/**',
+      '!node_modules/font-awesome/**/*.map',
+      '!node_modules/font-awesome/.npmignore',
+      '!node_modules/font-awesome/*.txt',
+      '!node_modules/font-awesome/*.md',
+      '!node_modules/font-awesome/*.json'
+    ])
+    .pipe(gulp.dest('vendor/font-awesome'))
 })
 
-// Define a new default task (so we can just call 'gulp' on the command line)
-// to automatically compile when we save changes to our Sass files
-gulp.task('default', function () {
-  gulp.watch(
-    sassPath, // which files to watch for changes (use our variable)
-    ['sass']) // an array of tasks to run when changes are detected.
+// Default task
+gulp.task('default', ['copy']);
+
+// Configure the browserSync task
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: ''
+    },
+  })
 })
+
+// Dev task with browserSync
+gulp.task('dev', ['browserSync'], function() {
+  // Reloads the browser whenever HTML or CSS files change
+  gulp.watch('css/*.css', browserSync.reload);
+  gulp.watch('*.html', browserSync.reload);
+});
