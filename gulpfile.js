8cc5c391ec4729a4d5142164d451879d0e3b5e0d
ogil0035 <32109851+ogil0035@users.noreply.github.com>
2017-11-28 15:52:43 -0500

    Tuesday
    Tueday Changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,30 @@
-var gulp = require('gulp');
+// Declare global scope variables.
+const gulp = require('gulp')      // load the gulp library
+const sass = require('gulp-sass') // load the gulp-sass compiler library
+const sassPath = './scss/**/*.scss' // the folder(s) with our Sass source files
+const postcss = require('gulp-postcss') // load the postcss library
+const autoprefixer = require('autoprefixer') // load the autoprefixer plugin
+const cssnano = require('cssnano') // load the cssnano plugin
+
 var browserSync = require('browser-sync').create();
 var pkg = require('./package.json');
+
+// Define a new task called 'sass' that we can call to compile Sass to CSS
+gulp.task('sass', function () {
+  // Create a plugins variable with the names and configuration parameters
+  // of the PostCSS plugins that we want to use.
+  const plugins = [
+    autoprefixer({ browsers: ['last 2 version'] }),
+    cssnano()
+  ]
+  // Now lets tell gulp what to do ...
+  return gulp
+    .src(sassPath)            // where to find the Sass source files (use our variable)
+    .pipe(sass())             // forward those files to the compiler
+    .pipe(gulp.dest('./css')) // where to output the interim compiled css
+    .pipe(postcss(plugins))   // apply the PostCSS plugins
+    .pipe(gulp.dest('./css/min')) // where to output the final minimized CSS files
+})
 
 // Copy vendor files from /node_modules into /vendor
 // NOTE: requires `npm install` before running!
@@ -45,6 +69,10 @@ gulp.task('browserSync', function() {
 // Dev task with browserSync
 gulp.task('dev', ['browserSync'], function() {
   // Reloads the browser whenever HTML or CSS files change
+  gulp.watch(
+    sassPath, // which files to watch for changes (use our variable)
+    ['sass'])
   gulp.watch('css/*.css', browserSync.reload);
   gulp.watch('*.html', browserSync.reload);
+    
 });
